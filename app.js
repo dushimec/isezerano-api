051db8774fe651bd5059e16773fa168fde2074e8
dushimec <dushimec515@gmail.com>
2024-09-06 23:41:07 +0200

    Changes for adding the initial route api

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(requestRateLimitConfig);
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+app.get("/", (req,res) =>{
+  res.send("Server is runing");
+})
+
+
 const api = process.env.API_URL;
 app.use(`${api}/auth`, authRoutes);
 app.use(`${api}/singers`, userRoutes);
